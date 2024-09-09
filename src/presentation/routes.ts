f79b5f80cd548d3b26ea9dcbfb3d89d858cbc2af
Dario Quispe <daro.qp@gmail.com>
2024-09-09 00:47:47 -0300

    feat: add routes

---
+++ src/presentation/routes.ts
@@ -0,0 +1,20 @@
+import { Router } from "express";
+
+
+export class AppRoutes {
+
+	static get routes(): Router {
+		const router = Router();
+
+		router.get('/api/todos', (_req, res) => {
+
+			res.json([
+				{ id: 1, text: 'Buy milk', createdAt: new Date() },
+				{ id: 2, text: 'Buy bread', createdAt: null },
+				{ id: 3, text: 'Buy coffee', createdAt: new Date() },
+			])
+		});
+
+		return router
+	}
+}
--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,9 +1,10 @@
-import express from 'express'
+import express, { Router } from 'express'
 import path from 'path';
 
 interface Options {
 	PORT: number;
 	PUBLIC_PATH?: string;
+	routes: Router;
 }
 
 export class Server {
@@ -11,12 +12,14 @@ export class Server {
 	private app = express();
 	private readonly port: number;
 	private readonly publicPath: string;
+	private readonly routes: Router;
 
 	constructor(options: Options) {
-		const { PORT, PUBLIC_PATH = 'public' } = options;
+		const { PORT, PUBLIC_PATH = 'public', routes } = options;
 
 		this.port = PORT;
 		this.publicPath = PUBLIC_PATH;
+		this.routes = routes;
 	}
 
 	async start() {
@@ -26,7 +29,11 @@ export class Server {
 		// Public Folder
 		this.app.use(express.static(this.publicPath));
 
-		this.app.get('*', (req, res) => {
+		// Routes
+		this.app.use(this.routes);
+
+		//* Comodin para SPA
+		this.app.get('*', (_req, res) => {
 			const indexPath = path.join(__dirname + `../../../${this.publicPath}/index.html`);
 			res.sendFile(indexPath);
 		})
