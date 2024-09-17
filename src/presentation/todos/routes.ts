a7d90821ddf178dd369c7a789e8412418b0351fc
Dario Quispe <daro.qp@gmail.com>
2024-09-17 00:02:11 -0300

    style: rename todo repository file

--- src/presentation/todos/routes.ts
+++ src/presentation/todos/routes.ts
@@ -1,7 +1,7 @@
 import { Router } from "express";
 import { TodosController } from "./controller";
 import { TodoDatasourceImpl } from "../../infrastructure/datasource/todo.datasource.impl";
-import { TodoRepositoryImpl } from "../../infrastructure/repositories/todo.repository";
+import { TodoRepositoryImpl } from "../../infrastructure/repositories/todo.repository.impl";
 
 
 export class TodoRoutes {
